2b32901e89901fbf6bff070831d5554f06ed11c7
Sudeep Kumar <140135072+SuDeEpKuMaR912@users.noreply.github.com>
2025-06-27 18:37:59 +0530

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,39 @@
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+
+iris= datasets.load_iris()
+
+#printing the details about the dataset
+'''print(iris.keys())
+dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename', 'data_module'])
+
+print(iris.target)
+print(iris.data)'''
+
+features= iris.data
+labels= iris.target
+
+#printing description of dataset
+'''print(features, labels)
+print(features[1], labels[1])
+print(iris.DESCR)'''
+
+#training the classifier
+clf= KNeighborsClassifier()
+clf.fit(features, labels)
+
+#getting data for new dataset
+print("\n\n")
+a=float(input('Enter SEPAL LENGTH: '))
+b=float(input('Enter SEPAL WIDTH: '))
+c=float(input('Enter PETAL LENGTH: '))
+d=float(input('Enter PETAL WIDTH: '))
+
+print("\n\nFollowing is how iris types are marked:\n\nIris Setosa- [0]\nIris Versicolour- [1]\nIris Virginia- [2]\n\n")
+
+newdata=[a,b,c,d]
+
+#getting classifications for new datasets
+pred=clf.predict([newdata])
+print('RESULT: ',pred) 
